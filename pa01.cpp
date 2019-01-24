a2835ab52e7f580e5c21fd678e61de6215acb0d7
Dr. Taylor <taylor@mst.ed>
2019-01-23 21:55:12 -0600

    Initial commit

---
+++ pa01.cpp
@@ -0,0 +1,14 @@
+/**
+Implement the main here to match input and output. 
+If you want to add extra functions, you can do so above main.
+**/
+
+#include "matrix_search.h"
+
+int main()
+{
+    // Write your main here.
+
+    return 0;
+}
+
